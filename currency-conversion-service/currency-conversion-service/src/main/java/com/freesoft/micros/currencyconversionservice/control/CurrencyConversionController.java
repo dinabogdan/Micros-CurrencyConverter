8bf78717d0be503ebaecade8dcb1a9e570554bb5
dinabogdan <bogdan.dina03@gmail.com>
2018-03-25 15:08:31 +0300

    add the endpoint for currency-exchange service from currency-converter service

--- currency-conversion-service/currency-conversion-service/src/main/java/com/freesoft/micros/currencyconversionservice/control/CurrencyConversionController.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/freesoft/micros/currencyconversionservice/control/CurrencyConversionController.java
@@ -1,11 +1,15 @@
 package com.freesoft.micros.currencyconversionservice.control;
 
 import com.freesoft.micros.currencyconversionservice.bean.CurrencyConversion;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -14,13 +18,27 @@ public class CurrencyConversionController {
     public CurrencyConversion convertCurrency(@PathVariable String fromCurrency,
                                               @PathVariable String toCurrency,
                                               @PathVariable BigDecimal quantity) {
-        return new CurrencyConversion(1L,
+
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("fromCurrency", fromCurrency);
+        uriVariables.put("toCurrency", toCurrency);
+        ResponseEntity<CurrencyConversion> responseEntity =
+                new RestTemplate().getForEntity(
+                        "http://localhost:8000//currency-exchange/from/{fromCurrency}/to/{toCurrency}",
+                        CurrencyConversion.class,
+                        uriVariables);
+
+        CurrencyConversion response = responseEntity.getBody();
+
+
+        return new CurrencyConversion(response.getId(),
                 fromCurrency,
                 toCurrency,
-                BigDecimal.valueOf(65),
+                response.getConversionMultiple(),
                 quantity,
-                quantity,
-                0);
+                quantity.multiply(response.getConversionMultiple()),
+                response.getPort());
 
     }
 }
