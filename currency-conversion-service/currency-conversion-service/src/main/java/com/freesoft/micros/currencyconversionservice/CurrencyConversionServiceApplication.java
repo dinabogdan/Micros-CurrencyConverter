bc669c12ade4a71be50dfed685446df518613922
dinabogdan <bogdan.dina03@gmail.com>
2018-03-31 16:52:48 +0300

    add sleuth

--- currency-conversion-service/currency-conversion-service/src/main/java/com/freesoft/micros/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/freesoft/micros/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,14 +1,21 @@
 package com.freesoft.micros.currencyconversionservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients("com.freesoft.micros.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CurrencyConversionServiceApplication.class, args);
